f59abed9119520c5cc1eabd829187dba95ee415c
phanuwit13 <big.phanuwit@gmail.com>
2022-06-11 18:11:35 +0700

    init gh pages

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
     'public', // path to public directory
     {
         branch: 'gh-pages',
-        repo: 'https://github.com/phanuwit13/portfolio.git', // Update to point to your repository  
+        repo: 'git@github.com:phanuwit13/portfolio.git', // Update to point to your repository  
         user: {
             name: 'phanuwit13', // update to use your name
             email: 'big.phanuwit@gmail.com' // Update to use your email
