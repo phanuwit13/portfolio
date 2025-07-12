5dceb65226088d9f5c377bd0eb9dac89d7602431
Phanuwit Kittirong <big.phanuwit@gmail.com>
2025-07-13 05:55:07 +0700

    add new design

---
+++ src/lib/components/EducationList.svelte
@@ -0,0 +1,52 @@
+<script lang="ts">
+	interface EducationItem {
+		degree: string;
+		school: string;
+		period: string;
+		details: string[];
+	}
+
+	interface AwardItem {
+		title: string;
+		year: string;
+		description: string;
+	}
+
+	export let education: EducationItem[];
+	export let awards: AwardItem[];
+</script>
+
+<div class="mt-8">
+	<h2 class="text-lg font-semibold">Education</h2>
+	<div class="mt-4 flex flex-col gap-4">
+		{#each education as edu}
+			<div class="mb-6">
+				<div class="flex flex-col md:flex-row md:justify-between">
+					<span class="font-semibold">{edu.degree}</span>
+					<span class="text-muted-foreground text-xs">{edu.period}</span>
+				</div>
+				<div class="text-muted-foreground italic">{edu.school}</div>
+				<ul class="mt-2 ml-5 list-disc space-y-1 text-sm">
+					{#each edu.details as det}
+						<li>{det}</li>
+					{/each}
+				</ul>
+			</div>
+		{/each}
+	</div>
+</div>
+
+<div class="mt-8">
+	<h2 class="text-lg font-semibold">Awards</h2>
+	<div class="mt-4 flex flex-col gap-4">
+		{#each awards as award}
+			<div class="mb-4">
+				<div class="flex flex-col md:flex-row md:justify-between">
+					<span class="font-semibold">{award.title}</span>
+					<span class="text-muted-foreground text-xs">{award.year}</span>
+				</div>
+				<div class="text-muted-foreground mt-1 text-sm italic">{award.description}</div>
+			</div>
+		{/each}
+	</div>
+</div>
---
+++ src/lib/components/TechStack.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+
+	export let techStack: { name: string; icon: typeof SvelteComponent; link: string }[];
+</script>
+
+<div class="mt-8">
+	<h2 class="text-lg font-semibold">Tech Stack</h2>
+	<div class="mt-4 grid grid-cols-2 gap-3 sm:grid-cols-3 md:grid-cols-4">
+    {#each techStack as tech}
+      <a href={tech.link} target="_blank" class="bg-muted dark:hover:bg-card flex cursor-pointer items-center justify-center gap-2 rounded-lg border border-dashed p-2 transition hover:bg-blue-50">
+        <tech.icon class="size-5" />
+        <span class="text-sm">{tech.name}</span>
+      </a>
+    {/each}
+  </div>
+</div>
