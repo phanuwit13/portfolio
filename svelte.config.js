0e5c0a7b77588dddee1816011c04fa3a044c72a0
Phanuwit Kittirong <big.phanuwit@gmail.com>
2025-07-13 07:09:34 +0700

    fix script

--- svelte.config.js
+++ svelte.config.js
@@ -15,10 +15,10 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			// pages: 'build',
+			// assets: 'build',
 			fallback: 'index.html', // ถ้าใช้ SPA, ถ้าไม่ใช้ก็ลบออกได้
-			precompress: false
+			// precompress: false
 		}),
 		paths: {
 			base: dev ? '' : `/${repo}`
